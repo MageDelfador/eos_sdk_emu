96943a2f6d77f81281d0a65b9e3698179c22547b
MageDelfador <9780339+MageDelfador@users.noreply.github.com>
2026-09-03 17:34:03 +0800

    Revert "aaa"
    This reverts commit c0b1e33879e462c44f110313f170f8fe3950d0a9.

--- extra/eos_sdk/eos_init1.19.0.h
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-#pragma once
-
-/** SDK 1.19 API bumps. Layout-compatible with the 1.16.4 structures. */
-#define EOS_INITIALIZE_API_005 5
-using EOS_InitializeOptions005 = EOS_InitializeOptions004;
-
-#define EOS_INITIALIZE_THREADAFFINITY_API_004 4
-using EOS_Initialize_ThreadAffinity004 = EOS_Initialize_ThreadAffinity003;
